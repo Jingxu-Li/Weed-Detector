f65cd4ddb8507202abd3f58299f5265b584220dd
Jingxu Li <bg2dph@outlook.com>
2023-02-13 22:35:08 +0800

    Fix README and pipeline functions
    Signed-off-by: Jingxu Li <bg2dph@outlook.com>

--- pipeline.py
+++ pipeline.py
@@ -1,20 +1,3 @@
 '''
 main pipeline of sprayer with depth camera
 '''
-
-from apscheduler.schedulers.blocking import BlockingScheduler
-
-from hardware.can_tool import sprayerCAN
-from detection.detector import detector
-
-det = detector()
-sprayer_can = sprayerCAN("COM5")
-
-def core_task():
-    det.detect()
-    sprayer_can.send_spray(1)
-
-if __name__ == '__main__':
-    scheduler = BlockingScheduler()
-    scheduler.add_job(core_task, "interval", seconds=2)
-    scheduler.start()
