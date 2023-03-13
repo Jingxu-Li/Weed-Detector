8e89a4a0ab2803096eab4f613574ecbd6971e0ca
Jingxu Li <bg2dph@outlook.com>
2023-03-13 10:46:38 +0800

    add init code for imu&gnss
    Signed-off-by: Jingxu Li <bg2dph@outlook.com>

---
+++ localization/pseudo_code.py
@@ -0,0 +1,18 @@
+delta_t = 0.01
+
+def get_status():
+    return x_k, y_k, z_k, roll_k, yaw_k, pitch_k
+
+def turn_gnss_to_distance():
+    distance = 0
+    return distance
+
+def predict(x_k, y_k, z_k, roll_k, yaw_k, pitch_k):
+    state = []
+    # update state
+    return
+
+def correct(x_gnss, y_gnss, a_imu, omega_imu, state):
+    # update state
+    correct_state = []
+    return correct_state
