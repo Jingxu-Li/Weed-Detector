18a016264a3588fc4bd4369ba46cd51ffa119de4
Jingxu Li <bg2dph@outlook.com>
2023-03-21 20:15:53 +0800

    can_tool more code, modify localization hard code

--- hardware/can_tool.py
+++ hardware/can_tool.py
@@ -6,6 +6,7 @@ This file aims to send/recv data from converter
 
 import socket
 import threading
+import time
 import random
 
 
@@ -27,17 +28,85 @@ class CANConverter():
         while True:
             receive_data, client = self.server_socket.recvfrom(1024)
             if receive_data:
-                print(f"Received data {receive_data} from {client}")
+                id = receive_data[1:5]
+                # 15: magic number, mask 1111
+                length = int.from_bytes(
+                    receive_data[0], byteorder='big', signed=False) & 15
+                print(
+                    f"Received data {receive_data[5:5 + length]} from id:{id}")
 
-    def send(self, message, server):
+    def send(self, message, server, id_int):
+        id = (id_int).to_bytes(4, byteorder='big')
         sub = 0
         length = len(message)
-        id = bytes([random.randint(0,255), random.randint(0,255), random.randint(0,255), random.randint(0,255)])
-        while length > 8 :
+        while length > 8:
             pre = bytes([168]) + id
             self.server_socket.sendto(pre+message[sub:sub + 8], server)
             sub = sub + 8
             length = length - 8
-        pre = bytes([160 + length]) + id
-        self.server_socket.sendto(pre + message[sub:sub + length] + bytes(8 - length), server)
 
+        # 160: magic number, indicates data frame
+        pre = bytes([160 + length]) + id
+        self.server_socket.sendto(
+            pre + message[sub:sub + length] + bytes(8 - length), server)
+
+
+class DriveFrame(CANConverter):
+    id = 0x00
+    drive_status = 0
+    cut_position = 0
+    motor_speed = 0
+    car_angle = 0
+    message = bytearray()
+
+    def __init__(self, host, port, id):
+        super().__init__(host, port)
+        self.id = id
+
+    def change_drive_status(self, target):
+        self.drive_status = target
+        self.compose_frame()
+
+    def change_motor_speed(self, speed):
+        self.motor_speed = speed
+        self.compose_frame()
+
+    def compose_frame(self):
+        self.message[1] = self.drive_status
+        self.message[3] = self.cut_position
+        # TODO: fulfill complete frame
+        self.send()
+
+class NozzleFrame(CANConverter):
+    id = 0x00
+    open_bits = 0x0000
+    message = bytearray()
+
+    def __init__(self, host, port, id):
+        super().__init__(host, port)
+        self.id = id
+
+    def change_drive_status(self, target):
+        self.drive_status = target
+        self.compose_frame()
+
+    def change_motor_speed(self, speed):
+        self.motor_speed = speed
+        self.compose_frame()
+
+    def compose_frame(self):
+        self.message[1] = self.open_bits[1]
+        # TODO: fulfill complete frame
+
+
+class Sprayer(NozzleFrame, DriveFrame):
+
+    def __init__(self, host, port):
+        super().__init__(host, port)
+
+    def forward(self, time):
+        self.change_drive_status(1)
+        time.sleep(time)
+        self.change_drive_status(3)
+        self.change_motor_speed(100)
+        time.sleep(time)
--- hardware/test.py
+++ hardware/test.py
@@ -1,5 +1,9 @@
 from hardware.can_tool import CANConverter
-
+from hardware.can_tool import BasicFrame
 con = CANConverter('192.168.1.62', 8080)
 con.send(b'12345678876578374875', ('192.168.1.10', 4001))
 con.receive()
+
+BasicFrame('192.168.1.62', 8080)
+BasicFrame.change_drive_status(1)
+assert BasicFrame.message == [xxxxxx]
