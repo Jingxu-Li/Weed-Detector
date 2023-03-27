9fc06c298ddabe1f0443808eedbc1e9ed92cdba6
M1M1cooL <603139027@qq.com>
2023-03-27 22:19:13 +0800

    develop driveFrame and sprayerFrame in can_tools

--- hardware/can_tool.py
+++ hardware/can_tool.py
@@ -6,7 +6,7 @@ This file aims to send/recv data from converter
 
 import socket
 import threading
-import random
+import struct
 
 
 class CANConverter():
@@ -16,6 +16,7 @@ class CANConverter():
     '''
 
     def __init__(self, host, port):
+        print(1)
         self.host = host
         self.port = port
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -27,17 +28,59 @@ class CANConverter():
         while True:
             receive_data, client = self.server_socket.recvfrom(1024)
             if receive_data:
-                print(f"Received data {receive_data} from {client}")
+                id = receive_data[1:5]
+                length = int.from_bytes(receive_data[0], byteorder='big', signed=False) & 15
+                print(f"Received data {receive_data[5:5 + length]} from id:{id}")
 
-    def send(self, message, server):
+    def send(self, message, server, int_id):
+        id = (int_id).to_bytes(4, byteorder='big')
         sub = 0
         length = len(message)
-        id = bytes([random.randint(0,255), random.randint(0,255), random.randint(0,255), random.randint(0,255)])
-        while length > 8 :
-            pre = bytes([168]) + id
-            self.server_socket.sendto(pre+message[sub:sub + 8], server)
-            sub = sub + 8
-            length = length - 8
+       # while length > 8:
+       #     pre = bytes([168]) + id
+       #     self.server_socket.sendto(pre + message[sub:sub + 8], server)
+       #      sub = sub + 8
+       #      length = length - 8
         pre = bytes([160 + length]) + id
         self.server_socket.sendto(pre + message[sub:sub + length] + bytes(8 - length), server)
 
+class driveFrame(CANConverter):
+    id = 0
+    drive_status = 0
+    header_percent = 0
+    engine_rpm = 0
+    turn_angle = 0
+    message = bytes(8)
+    def __init__(self, host, port, id):
+        super().__init__(host, port)
+        self.id = id
+    def change_drive_status(self, drive_status):
+        self.message = self.message[0:1]+bytes([drive_status])+self.message[2:8]
+
+    def change_header_percent(self, header_percent):
+        self.message = self.message[0:3]+bytes([header_percent])+self.message[4:8]
+
+    def change_engine_rpm(self, engine_rpm):
+        self.message = self.message[0:4]+(engine_rpm).to_bytes(2, byteorder='big')+self.message[6:8]
+
+    def change_turn_angle(self,turn_angle):
+        self.message = self.message[0:6]+bytes([turn_angle])+self.message[7:8]
+
+    def send(self, server):
+        super().send(self.message, server, self.id)
+
+class sprayerFrame(CANConverter):
+    message = bytes(8)
+    id = 0
+    def __init__(self, host, port, id):
+        super().__init__(host, port)
+        self.id = id
+    def change_spray_head(self, msg):
+        if len(msg == 22):
+            temp = 0
+            for item in msg:
+                temp = temp*2+item
+        self.message = ((int)(temp*4)).to_bytes(3, byteorder='big') + self.message[3:8]
+    def send(self, server):
+        print(self.message)
+        super().send(self.message, server, self.id)
--- hardware/test.py
+++ hardware/test.py
@@ -1,5 +1,22 @@
-from hardware.can_tool import CANConverter
+import numpy as np
 
-con = CANConverter('192.168.1.62', 8080)
-con.send(b'12345678876578374875', ('192.168.1.10', 4001))
-con.receive()
+from hardware.can_tool import CANConverter, driveFrame, sprayerFrame
+#con = CANConverter('192.168.1.62', 8080)
+#con.send(b'12345678876578374875', ('192.168.1.10', 4001), 0x0CFF2481)
+#con.receive()
+
+con = driveFrame('192.168.1.62', 8080, 0x0201)
+drive_status = 3
+header_percent = 98
+engine_rpm = 0x1111
+turn_angle = 155
+con.change_drive_status(drive_status)
+con.change_header_percent(header_percent)
+con.change_engine_rpm(engine_rpm)
+con.change_turn_angle(turn_angle)
+con.send('192.111.111.11')
+con1 = sprayerFrame('192.168.1.62', 8080, 0x0202)
+b = np.zeros(22, dtype=int)
+b[4:8]=1
+con1.change_spray_head(b)
+con1.send('11111')
