982f81745dc7a253a3cd293297be1c224befbca2
Jingxu Li <bg2dph@outlook.com>
2023-06-05 17:12:20 +0800

    upload code after the war
    Signed-off-by: Jingxu Li <bg2dph@outlook.com>

--- detection/detector.py
+++ detection/detector.py
@@ -2,7 +2,69 @@
 Main algorithm implementation for sprayer detector
 '''
 
+import cv2
+from imutils import grab_contours
+import numpy as np
+import matplotlib.pyplot as plt
+
 class Detector():
+
     def __init__(self):
         pass
-        
+
+    def exg_img(self, img):
+        low = 50
+        high = 200
+        blue = img[:, :, 0].astype(np.float32)
+        green = img[:, :, 1].astype(np.float32)
+        red = img[:, :, 2].astype(np.float32)
+        imgOut = 2 * green - red - blue
+        imgOut = np.clip(imgOut, 0, 255)
+        imgOut = imgOut.astype('uint8')
+        mask = cv2.inRange(imgOut, low, high)
+        return mask
+
+    def find_weeds_threshold(self, img, store=False, port=None):
+        img_binary = self.exg_img(img)
+
+        # plt.imshow(img_binary)
+        # plt.show()
+
+        # remove holes in the surface
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (6, 6))
+        open_res = cv2.morphologyEx(img_binary, cv2.MORPH_CLOSE, kernel, iterations=5)
+        items = cv2.findContours(open_res, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # print(contours)
+        # cnts = items[0] if len(items) == 2 else items[1]
+        counts = grab_contours(items)
+
+        weedCenters = []
+        if len(counts) != 0:
+            for cnt in counts:
+                startX, startY, boxW, boxH = cv2.boundingRect(cnt)
+                endX = startX + boxW
+                endY = startY + boxH
+                centerX = int(startX + (boxW / 2))
+                centerY = int(startY + (boxH / 2))
+                weedCenters.append([centerX, centerY])
+                result = cv2.rectangle(img, (int(startX), int(startY)), (endX, endY), (0, 0, 255), 2)
+        # if weedCenters:
+        #     plt.imshow(result)
+        #     plt.show()
+        if store:
+            import time
+            current_time = time.time()
+            cv2.imwrite(f"./log_pics/port_{port}_{current_time}_raw.jpg", img)
+            if weedCenters:
+                cv2.imwrite(f"./log_pics/port_{port}_{current_time}_read.jpg", result)
+            else:
+                # print("Not detected")
+                cv2.imwrite(f"./log_pics/port_{port}_{current_time}_not.jpg", img_binary)
+        return weedCenters
+
+if __name__ == "__main__":
+    det = Detector()
+    img = cv2.VideoCapture(1)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    centers = det.find_weeds_threshold(img, store=True, port=1)
+    print(centers)
--- pipeline.py
+++ pipeline.py
@@ -1,79 +1,120 @@
 '''
 main pipeline of sprayer with single camera
 '''
-
+from copy import deepcopy
 import cv2
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
-import sys
 import time
 
 from hardware.can_tool import sprayerFrame
+from detection.detector import Detector
+import queue
+import threading
 
-debug = False
+width = 640
 
-def initial_cap():
-    cap = cv2.VideoCapture(1 + cv2.CAP_DSHOW)
-    flag = cap.isOpened()
-    if not flag:
-        print("Camera is not opened")
-    else:
-        print("Camera opened")
-        return cap
+map_nuzzle = {
+    0:[5,6,7],
+    1:[2,3,4],
+    2:[15,0,1],
+    3:[11, 12, 14, 13],
+    4:[8,9,10],
+    5:[21, 16, 17],
+    6:[18,19,20],
+}
 
-def get_image(cap, debug_mode):
+def get_image(video_port):
+    cap = cv2.VideoCapture(video_port, cv2.CAP_V4L2)
     frame = None
-    if debug_mode:
-        frame = cv2.imread('./detection/example.jpg')
-    else:
-        try:
-            ret, frame = cap.read()
-        except Exception as e:
-            print(e)
+    try:
+        ret, frame = cap.read()
+    except Exception as e:
+        print(e)
+        return None
     return frame
 
+def set_left(arr, port, value):
+    if len(map_nuzzle[port])==4:
+        arr[map_nuzzle[port][0]]=value
+        arr[map_nuzzle[port][1]]=value
+    else:
+        arr[map_nuzzle[port][0]]=value
+        arr[map_nuzzle[port][1]]=value
+    return arr
+
+def set_right(arr, port, value):
+    if len(map_nuzzle[port])==4:
+        arr[map_nuzzle[port][2]]=value
+        arr[map_nuzzle[port][3]]=value
+    else:
+        arr[map_nuzzle[port][1]]=value
+        arr[map_nuzzle[port][2]]=value
+    return arr
+
+def spray_without_threads(port, arr, debug=False):
+    img = get_image(port)
+    if img is None:
+        return None
+    if not img.any():
+        return None
+    centers = det.find_weeds_threshold(img, True, port)
+    if centers:
+        print(f"Found in port:{port}")
+        left = False
+        right = False
+        for center in centers:
+            print(center)
+            if center[0] < width/2:
+                left = True
+                arr = set_left(arr,port, 1)
+            else:
+                right = True
+                arr = set_right(arr,port,1)
+        if not left:
+            arr = set_left(arr,port,0)
+        if not right:
+            arr = set_right(arr,port,0)
+    return arr
+
+q = queue.Queue()
+
+def send_to_sprayer():
+    while True:
+        try:
+            cmd = q.get()
+            con.change_spray_head(cmd)
+            con.send_message(server)
+            print(f"Have sent to server:{cmd}")
+            time.sleep(2)
+            con.change_spray_head(zero_msg)
+            con.send_message(server)
+        except:
+            continue
+        
+
+def prt_cmd(cmd):
+    indexes = []
+    for i in range(0,len(cmd)):
+        if cmd[i]:
+            indexes.append(i)
+    print(f"indexes:{indexes}")
+
 if __name__ == "__main__":
-    con1 = sprayerFrame('192.168.1.6', 8001, 0x0202)
-    b = np.zeros(22, dtype=int)
-    a = np.zeros(22, dtype=int)
-    b[4:8]=1
-    server = ('192.168.1.10', 4001)
 
+    debug = False
     if not debug:
-        cap = initial_cap()
-    else:
-        cap = None
-
-    img = get_image(cap, debug)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    plt.imshow(np.asarray(img))
-    color_low = np.array([190, 190, 190])
-    color_high = np.array([255, 255, 255])
-    mask = cv2.inRange(img, color_low, color_high)
-    # plt.imshow(np.asarray(mask))
-
-    # remove holes in the surface
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-    open_res = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=2)
-    # plt.imshow(np.asarray(open_res))
-    plt.show()
-    contours, _ = cv2.findContours(open_res, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    area = []
-
-    if len(contours) != 0:
-        for i in range(len(contours)):
-            area.append(cv2.contourArea(contours[i]))
-        max_idx = np.argmax(area)
-        con1.change_spray_head(b)
-        con1.send_message(server)
-        time.sleep(5)
-        con1.change_spray_head(a)
-        con1.send_message(server)
-    else:
-        sys.exit(0)
-
-    min_box = cv2.minAreaRect(contours[max_idx])
-    print(min_box)
-    pts = np.int0(cv2.boxPoints(min_box))
-    cv2.drawContours(open_res, [pts], 0, (255, 0, 0), 3)
-    cv2.imwrite("result.jpg", open_res)
+        con = sprayerFrame('192.168.1.6', 8001, 0x0202)
+    server = ('192.168.1.10', 4001)
+    ports = [0,1,2,3,4,5]
+    det = Detector()
+    zero_msg = np.zeros(22, dtype=int)
+    spray_command = np.zeros(22, dtype=int)
+    t = threading.Thread(target=send_to_sprayer, daemon=True)
+    t.start()
+    while True:
+        for port in ports:
+            spray_command = spray_without_threads(port, spray_command, debug)
+            spray_cmd_cp = deepcopy(spray_command)
+            q.put(spray_cmd_cp)
+        print(spray_command)
